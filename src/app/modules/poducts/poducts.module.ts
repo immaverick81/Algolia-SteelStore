1615ef782730c65601327e68b120378b985c0a9e
Sabya Sachi <Sabya.Sachi@verscend.com>
2020-08-24 23:34:00 +0530

    clear functionality update

--- src/app/modules/poducts/poducts.module.ts
+++ src/app/modules/poducts/poducts.module.ts
@@ -1,7 +1,9 @@
+import { AlgoliaClearFilterComponent } from './../../shared/algolia-clear-filter/algolia-clear-filter.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -10,6 +12,9 @@ import { NgAisModule, NgAisInstantSearch } from 'angular-instantsearch';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 import { Ng5SliderModule } from 'ng5-slider';
+
+import { MenuSelectComponent } from 'src/app/shared/algolia-menu-select/algolia-menu-select.component';
+import { AlgoliaToggleComponent } from 'src/app/shared/algolia-toggle/algolia-toggle.component';
 
 import { AlgoliaRangeComponent } from './../../shared/algolia-range/algolia-range.component';
 
@@ -25,33 +30,35 @@ import { HotrollComponent } from './hotroll/hotroll.component';
 import { ListingComponent } from './listing/listing.component';
 import { ProductfiltersComponent } from './productfilters/productfilters.component';
 import { ProductmainwraaperComponent } from './productmainwraaper/productmainwraaper.component';
-import { MenuSelectComponent } from 'src/app/shared/algolia-menu-select/algolia-menu-select.component';
 
 @NgModule({
-  declarations: [
-    ProductmainwraaperComponent,
-    GalvanizedComponent,
-    GalvalumeComponent,
-    HotrollComponent,
-    ColdRollComponent,
-    ElectrogalvanizedComponent,
-    GalvalenalComponent,
-    ProductfiltersComponent,
-    DetailsComponent,
-    AlgoliaRangeComponent,
-    ListingComponent,
-    MenuSelectComponent,
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    PoductsRoutingModule,
-    NgbModule,
-    MatTabsModule,
-    Ng5SliderModule,
-    MatExpansionModule,
-    NgAisModule,
-  ],
-  providers: [NgAisInstantSearch],
+	declarations: [
+		ProductmainwraaperComponent,
+		GalvanizedComponent,
+		GalvalumeComponent,
+		HotrollComponent,
+		ColdRollComponent,
+		ElectrogalvanizedComponent,
+		GalvalenalComponent,
+		ProductfiltersComponent,
+		DetailsComponent,
+		AlgoliaRangeComponent,
+		ListingComponent,
+		MenuSelectComponent,
+		AlgoliaToggleComponent,
+		AlgoliaClearFilterComponent
+	],
+	imports: [
+		CommonModule,
+		SharedModule,
+		PoductsRoutingModule,
+		NgbModule,
+		MatTabsModule,
+		Ng5SliderModule,
+		MatExpansionModule,
+		NgAisModule,
+		MatCheckboxModule
+	],
+	providers: [ NgAisInstantSearch ]
 })
 export class PoductsModule {}
