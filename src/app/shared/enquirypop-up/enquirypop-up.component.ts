7dac978e55e4625253f94274141c65e00f106b26
Prithwiraj Banerjee <Prithwiraj.Banerjee@cotiviti.com>
2020-08-25 21:33:22 +0530

    selected product detail is sent to enuirypopup component when dailog is opened

--- src/app/shared/enquirypop-up/enquirypop-up.component.ts
+++ src/app/shared/enquirypop-up/enquirypop-up.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-enquirypop-up',
@@ -8,8 +8,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class EnquirypopUpComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<EnquirypopUpComponent> ) { }
-
+  constructor(public dialogRef: MatDialogRef<EnquirypopUpComponent>, @Inject(MAT_DIALOG_DATA) public selectedProduct ) { }
   ngOnInit(): void {
   }
   closedialog(){
