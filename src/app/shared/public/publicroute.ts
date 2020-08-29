b7e50ab3f5755d5ed44b77806b2cc6228f6c7277
Sabya Sachi <Sabya.Sachi@verscend.com>
2020-08-29 22:16:41 +0530

    merged lastest changes

--- src/app/shared/public/publicroute.ts
+++ src/app/shared/public/publicroute.ts
@@ -1,47 +1,43 @@
-import { Routes, RouterModule } from "@angular/router";
-import { RouteConstant } from "../../constants/routeConstant";
+import { Routes, RouterModule } from '@angular/router';
+
 import { AboutComponent } from '../about/about.component';
 import { FaqComponent } from '../faq/faq.component';
 import { PrivacyComponent } from '../privacy/privacy.component';
-
+import { RouteConstant } from '../../constants/routeConstant';
 
 export const PUBLIC_ROUTES: Routes = [
-  // { path: '', redirectTo: 'account', pathMatch: 'full' },
+	// { path: '', redirectTo: 'account', pathMatch: 'full' },
 
-  {
-    path: "",
-     loadChildren: () =>
-       import("../../modules/home/home.module").then((m) => m.HomeModule),
- },
+	{
+		path: '',
+		loadChildren: () => import('../../modules/home/home.module').then((m) => m.HomeModule)
+	}
 
- {
-   path: 'steel',
-   loadChildren: () =>
-   import("../../modules/poducts/poducts.module").then((m) => m.PoductsModule),
- },
- {
-  path: '',
-  loadChildren: () =>
-  import("../../modules/other/other.module").then((m) => m.OtherModule),
-},
- {
-    path: RouteConstant.aboutUs,
-    component: AboutComponent
-  },
-  {
-    path: RouteConstant.faq,
-    component: FaqComponent
-  },
-  {
-    path: RouteConstant.privacyPolicy,
-    component: PrivacyComponent
-  },  
-  {
-    path: "",
-     loadChildren: () =>
-       import("../../modules/other/other.module").then((m) => m.OtherModule),
- },
-
-  
-  
+	//  {
+	//    path: 'steel',
+	//    loadChildren: () =>
+	//    import("../../modules/poducts/poducts.module").then((m) => m.PoductsModule),
+	//  },
+	//  {
+	//   path: '',
+	//   loadChildren: () =>
+	//   import("../../modules/other/other.module").then((m) => m.OtherModule),
+	// },
+	//  {
+	//     path: RouteConstant.aboutUs,
+	//     component: AboutComponent
+	//   },
+	//   {
+	//     path: RouteConstant.faq,
+	//     component: FaqComponent
+	//   },
+	//   {
+	//     path: RouteConstant.privacyPolicy,
+	//     component: PrivacyComponent
+	//   },
+	//   {
+	//     path: "",
+	//      loadChildren: () =>
+	//        import("../../modules/other/other.module").then((m) => m.OtherModule),
+	//  },
 ];
