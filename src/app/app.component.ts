c70c8aea460e8e3981dc57ed24be3f520e2d925d
Sabya Sachi <Sabya.Sachi@verscend.com>
2020-09-01 22:40:44 +0530

    google analytics integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
+import { Router, NavigationEnd } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
 import * as AOS from 'aos';
 
+// tslint:disable-next-line:ban-types
+declare let ga: Function;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent  implements OnInit {
-  title = 'exos';
+export class AppComponent implements OnInit {
+	title = 'exos';
+	constructor(public router: Router) {
+		this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				ga('set', event.urlAfterRedirects);
+				ga('send', 'pageview');
+			}
+		});
+	}
 
-  ngOnInit(){
-    AOS.init({
-     
-      easing: 'ease-in-sine',
-      delay: 100,
-    });
-
-
-  }
+	ngOnInit() {
+		AOS.init({
+			easing: 'ease-in-sine',
+			delay: 100
+		});
+	}
 }
