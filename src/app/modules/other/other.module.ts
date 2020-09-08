cf4652f67f9d147b04bf4d7bde743ce1ffe9a26d
Appventurez <adnan@appventurez.com>
2020-09-09 01:19:20 +0530

    jk

--- src/app/modules/other/other.module.ts
+++ src/app/modules/other/other.module.ts
@@ -5,10 +5,14 @@ import { OtherRoutingModule } from './other-routing.module';
 import { BlogsComponent } from './blogs/blogs.component';
 import { LiteratureComponent } from './literature/literature.component';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { BlogDetails1Component } from './blogs/blog-details1/blog-details1.component';
+import { BlogDetails2Component } from './blogs/blog-details2/blog-details2.component';
+import { BlogDetails3Component } from './blogs/blog-details3/blog-details3.component';
+import { TermsComponent } from './terms/terms.component';
 
 
 @NgModule({
-  declarations: [BlogsComponent, LiteratureComponent],
+  declarations: [BlogsComponent, LiteratureComponent, BlogDetails1Component, BlogDetails2Component, BlogDetails3Component, TermsComponent],
   imports: [
     CommonModule,
     MatExpansionModule,
